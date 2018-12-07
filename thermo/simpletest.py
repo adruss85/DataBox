cd80ad2dfdcb746885df3c3ebc5477e2cbaf6784
Adam Russell <adruss85@gmail.com>
2018-12-07 17:22:50 +0000

    added datetime in thermo

--- thermo/simpletest.py
+++ thermo/simpletest.py
@@ -24,12 +24,15 @@
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
+import os
 import time
-
+import datetime
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
 
-
+f = open("data.txt", "w")
+f.write("Time,Temp Channel 1 (*C)\n")
+f.close()
 # Define a function to convert celsius to fahrenheit.
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
@@ -67,4 +70,8 @@ while True:
     internal = sensor.readInternalC()
     print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
     print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
-    time.sleep(5.0)
+    now = time.strftime('%d-%m-%Y %H:%M:%S.%f')
+    f = open("data.txt", "a")
+    f.write(now + ',{0:0.3F}\n'.format(temp, c_to_f(temp)))
+    f.close
+    time.sleep(0.5)
