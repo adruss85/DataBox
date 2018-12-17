2988bf70c93d446f632896cabe62d3f6e482409e
adruss85 <adruss85@gmail.com>
2018-12-17 08:50:06 +0000

    Successfully sending data to the test database.  Included freetds config files in repo

--- Database/connection.py
+++ Database/connection.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
 #
 import pyodbc
+import datetime
 
 print('Connecting to database...\n')
 
 con = pyodbc.connect("DSN=RIVWARE;UID=dataguys;PWD=dataguys;TDS_Version=4.2")
-
+cursor = con.cursor()
 print(con)
+print('Connected')
 
+now =  datetime.datetime.now()
+print(now)
+
+ID = '1'
+Temp = '23'
+Force = '32.4324'
+
+cursor.execute("INSERT INTO dbo.Data ([Date Time], ID, Force, Temperature) VALUES (?, ?, ?, ?)", now, ID, Force, Temp) 
+con.commit()
+
+con.close()
